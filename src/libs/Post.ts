34961733929464157266d2dca9c19fd656925e2a
Scott O'Brien <scott@scottyob.com>
2024-12-02 15:16:38 -0800

    Basic blog post content navigation working

--- src/libs/Post.ts
+++ src/libs/Post.ts
@@ -1,25 +1,32 @@
-import { getCollection } from 'astro:content';
+import type { AstroComponentFactory } from 'astro/runtime/server/index.js';
+import { getCollection, type CollectionEntry, type InferEntrySchema, type Render } from 'astro:content';
 import { marked } from 'marked';
 
 const EXCERPT_SEPERATOR = '{/* --- */}';
 
-function getExcerpt(content: string, length=100) {
+function getExcerpt(content: string, length = 100) {
     let excerpt = content.split(' ').slice(0, length).join(' ') + '...';
-    if(content.includes(EXCERPT_SEPERATOR)) {
+    if (content.includes(EXCERPT_SEPERATOR)) {
         excerpt = content.split(EXCERPT_SEPERATOR)[0].trim();
     }
-    
+
     return marked.parse(excerpt);
 }
 
-export async function getAllPosts() {
+export interface Post {
+    content: CollectionEntry<'posts'>,
+    data: InferEntrySchema<'posts'>,
+    excerpt: string | Promise<string>,
+    slug: string
+}
+
+export async function getAllPosts(): Promise<Post[]> {
     const allBlogPosts = (await getCollection('posts'));
 
     const pages = await Promise.all(allBlogPosts.map(async (post) => {
-        const renderedContent = await post.render();
 
         return {
-            "content": renderedContent,
+            "content": post,
             "data": post.data,
             "excerpt": getExcerpt(post.body),
             "slug": post.slug,
