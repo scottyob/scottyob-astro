c8c0de33f59721e163d0f2ef478da232ff8e3ee9
Scott O'Brien <scott@scottyob.com>
2024-12-05 09:59:30 -0800

    Added bucket list and recipe collection

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,24 +1,39 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z, type ImageFunction } from 'astro:content';
+
+// Define common styles
+const ArticleStyles: readonly [string, ...string[]] = ['img-left', 'img-top', 'img-between'];
+
+// Define a base schema with common fields
+const baseSchema = (image: ImageFunction) => z.object({
+  title: z.string(),
+  hero: image().optional(),
+  style: z.enum(ArticleStyles).default('img-left'),
+});
+
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
-    type: "content",
-    schema: ({image}) => z.object({
-        // TODO:  Consider expanding with some examples of https://docs.astro.build/en/guides/content-collections/#defining-datatypes-with-zod
-        title: z.string(),
-        description: z.string().optional(),
-        tags: z.array(z.string()).optional(),
-        image: z.string().optional(),
-        date: z.date(),
-        hero: image().optional(),
-        forceHeroOnTop: z.boolean().optional(),
-        heroObjectPosition: z.string().optional(),
-        style: z.enum(['img-left', 'img-top', 'img-between']).default('img-left'),
-        draft: z.boolean().optional(),
-    })
+  type: "content",
+  schema: ({ image }) => baseSchema(image).extend({
+    description: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+    date: z.date(),
+    forceHeroOnTop: z.boolean().optional(),
+    heroObjectPosition: z.string().optional(),
+    draft: z.boolean().optional(),
+  })
 });
+
+// Recipe collection
+const recipeCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => baseSchema(image).extend({
+    categories: z.array(z.string()).optional(),
+  })
+});
+
 // 3. Export a single `collections` object to register your collection(s)
-//    This key should match your collection directory name in "src/content"
 export const collections = {
   'posts': blogCollection,
+  'recipes': recipeCollection,
 };
---
+++ src/pages/bucketlist/Item.react.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { Modal } from 'pretty-modal'
+import { useState } from 'react';
+
+export type Props = {
+  title: string;
+  children?: string | JSX.Element | JSX.Element[];
+}
+
+export default function Item(props: Props) {
+  const { title } = props;
+  const [isOpen, setIsOpen] = useState(false);
+
+  if (props.children === undefined) {
+    return <li>{title}</li>
+  }
+
+  return <>
+    <Modal open={isOpen} onClose={() => { setIsOpen(false) }}>
+      <div className="flex justify-end">
+        <button
+          className="bg-white hover:bg-gray-100 text-gray-800 font-semibold px-1 mb-4 border border-gray-200 rounded shadow block "
+          onClick={() => {
+            setIsOpen(false)
+          }}
+        >
+          X
+        </button>
+      </div>
+      <div className='max-w-2xl prose'>
+        {props.children}
+      </div>
+    </Modal>
+    <li>{title}{" "}
+      <a href="" onClick={(event) => { setIsOpen(true); event.preventDefault(); }}>...
+      </a>
+    </li>
+
+  </>
+
+}
