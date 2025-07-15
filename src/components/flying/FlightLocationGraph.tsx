4323b0db3a5f4b7d37b7c8fa1bd0d16febcb4280
Scott O'Brien <scott@scottyob.com>
2025-07-15 17:42:07 +0200

    Allow flying by country

--- src/components/flying/FlightLocationGraph.tsx
+++ src/components/flying/FlightLocationGraph.tsx
@@ -1,5 +1,6 @@
 import type { Flight } from '@libs/flyingTypes';
 import { Bar } from '@nivo/bar';
+import { useState } from 'react';
 import AutoSizer from 'react-virtualized-auto-sizer';
 
 export type Props = {
@@ -7,12 +8,15 @@ export type Props = {
   flights: Flight[];
 };
 
-const getData = (flights: Flight[]) => {
+const getData = (flights: Flight[], byCountry: boolean = false) => {
   const result: { [location: string]: { [year: string]: number } } =
     flights.reduce(
       (acc: { [location: string]: { [year: string]: number } }, flight) => {
         const year = flight.date.split('-')[0]; // extract year from date
-        const location = flight.location || 'Unknown'; // use 'Unknown' if location is undefined
+        let location = flight.location || 'Unknown'; // use 'Unknown' if location is undefined
+        if(byCountry) {
+          location = flight.launch?.country || 'Unknown'; // use 'Unknown' if location is undefined
+        }
         if (!acc[location]) {
           acc[location] = {};
         }
@@ -44,12 +48,17 @@ const getData = (flights: Flight[]) => {
 export default function FlightYearGraph(props: Props) {
   const { flights } = props;
 
-  const data = getData(flights);
+  const [byCountry, setByCountry] = useState(false);
+
+  // Recompute data when flights or byCountry changes
+  const data = getData(flights, byCountry);
+
+  const years = Array.from(new Set(flights.map(f => f.date.substring(0, 4))));
 
   const graph = (width: number) => (
     <Bar
       data={data}
-      keys={Array.from(new Set(flights.map(f => f.date.substring(0, 4)))) }
+      keys={years}
       colorBy={"id"}
       totalsOffset={10}
       enableTotals
@@ -78,11 +87,26 @@ export default function FlightYearGraph(props: Props) {
   );
 
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ flex: '1 1 auto' }}>
-        <AutoSizer disableHeight>
-          {({ width }) => <>{graph(width)}</>}
-        </AutoSizer>
+    <div>
+      <div className="text-right mr-4">
+        <label className="inline-flex items-center cursor-pointer">
+          <input
+            type="checkbox"
+            checked={byCountry}
+            className="sr-only peer"
+            onChange={(event) => setByCountry(event.target.checked)}
+          />
+          <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-orange-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-orange-600 dark:peer-checked:bg-orange-600"></div>
+          <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">By Country</span>
+        </label>
+      </div>
+
+      <div style={{ display: 'flex' }}>
+        <div style={{ flex: '1 1 auto' }}>
+          <AutoSizer disableHeight>
+            {({ width }) => <>{graph(width)}</>}
+          </AutoSizer>
+        </div>
       </div>
     </div>
   );
